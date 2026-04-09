a10c7cd085cb7f4c71852d386708cf9e0a8c5240
prateekyado <yadavprateek2406@gmail.com>
2026-04-09 17:11:26 +0530

    Updated app.run for deployment

--- main.py
+++ main.py
@@ -110,4 +110,5 @@ def matcher():
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True)
+
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
